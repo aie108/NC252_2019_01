1a7340f2290e07aa38d54a1c6500e61316849eaa
aie108 <aie108@gmail.com>
2019-08-13 16:24:22 +0700

    Add files via upload
    อัพโหลดครั้งแรก งานยังไม่เสร็จ

---
+++ NC252-2019/src/th/ac/utcc/cpe/nc252/suparerk/RegistrationRecord.java
@@ -0,0 +1,38 @@
+package th.ac.utcc.cpe.nc252.suparerk;
+
+public class RegistrationRecord {
+	private Student student;
+	private Subject [] subjects;
+	private int count;
+	
+	public RegistrationRecord() {
+		count = 0;
+		student = new Student();
+		subjects = new Subject[10];
+		for(int i=0; i < 10; i++) {
+			subjects[i] = new Subject();
+		}
+	}
+	
+	public RegistrationRecord(int n) {
+		count = 0;
+		student = new Student();
+		subjects = new Subject[n];
+		for(int i=0; i < n; i++) {
+			subjects[i] = new Subject();
+		}
+	}
+	
+	public void addSubject(Subject s) {
+		if(count < subjects.length - 1) {
+			subjects[count] = s;
+			count++;
+		}
+	}
+	
+	public void display() {
+		for(int i=0; i < count; i++) {
+			System.out.println(subjects[i]);
+		}
+	}
+}
---
+++ NC252-2019/src/th/ac/utcc/cpe/nc252/suparerk/Student.java
@@ -0,0 +1,35 @@
+package th.ac.utcc.cpe.nc252.suparerk;
+
+public class Student {
+	private String id;
+	private String name;
+	private String tel;
+	private String email;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
